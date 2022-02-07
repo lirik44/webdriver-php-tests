96f7999f41ae99d891db805d27698888becf3e22
lirik44 <sharapov.k44@gmail.com>
2022-02-08 00:23:05 +0300

    -del consts.local.php
    -del consts.local.php

--- consts.local.php
+++
@@ -1,7 +0,0 @@
-<?php
-const HOST = 'www.w3schools.com/sql/trysql.asp?filename=trysql_select_all';
-const HTTP_SCHEMA = 'http://';
-const HTTPS_SCHEMA = 'https://';
-const LINK_SELENIUM = 'http://localhost:4444/wd/hub';
-const BROWSER = 'chrome';
-//const BROWSER = 'firefox';
